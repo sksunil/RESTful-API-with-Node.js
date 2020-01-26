4e4498d52001d0b2a6185591cf82f4151fc92781
sksunil <sksunilkumawat1995@gmail.com>
2020-01-27 00:05:09 +0530

    controller added to the project

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const Product = require('../models/product')
 const multer = require('multer')
+const ProductsController = require('../controllers/products');
+
 //storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -28,92 +28,10 @@ const upload = multer({
 //Router is a sub package of the express
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    Product.find()
-        .select("name price _id productImage")
-        .exec()
-        .then(docs => {
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            res.status(500)
-                .json({
-                    error: err
-                })
-        })
-});
-
-router.post('/', upload.single('productImage'), (req, res, next) => {
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price,
-        productImage: req.file.path
-    });
-    product.save()
-        .then(result => {
-            res.status(200)
-                .json({
-                    result: result
-                })
-        })
-        .catch(err => {
-            res.status(500)
-                .json({
-                    error: err
-                })
-        })
-});
-
-router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    Product.update({ _id: id },
-        {
-            $set: {
-                name: req.body.name,
-                price: req.body.price
-            }
-        })
-        .exec()
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(err => {
-            res.status(500)
-                .json({
-                    error: err
-                })
-        })
-});
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    Product.findById(id)
-        .exec()
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(err => {
-            res.status(500)
-                .json({
-                    error: err
-                })
-        })
-});
-
-router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    Product.remove({ _id: id })
-        .exec()
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(err => {
-            res.status(500)
-                .json({
-                    error: err
-                })
-        })
-});
+router.get("/", ProductsController.products_get_all)
+router.post("/", upload.single('productImage'), ProductsController.products_create_product);
+router.get("/:productId", ProductsController.products_get_product);
+router.patch("/:productId", ProductsController.products_update_product);
+router.delete("/:productId", ProductsController.products_delete);
 
 module.exports = router;
