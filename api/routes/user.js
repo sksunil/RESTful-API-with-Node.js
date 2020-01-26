aa48c5a1947b209297b083267b96357ce27e804e
sksunil <sksunilkumawat1995@gmail.com>
2020-01-26 22:53:45 +0530

    added jwt token generation

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
 const User = require('../models/user');
 
 router.post('/singup', (req, res, next) => {
@@ -32,6 +34,52 @@ router.post('/singup', (req, res, next) => {
                 });
         }
     });
+})
+
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401)
+                    .json({
+                        message: "Auth failed"
+                    })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401)
+                        .json({
+                            message: "Auth failed"
+                        });
+                }
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id,
+                            password: req.body.password
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: '1h'
+                        }
+                    );
+                    return res.status(200)
+                        .json({
+                            message: "Auth successful",
+                            token: token
+                        });
+                }
+
+                res.status(401)
+                    .json({
+                        message: "Auth failed"
+                    });
+            })
+
+        })
+        .catch();
 })
 
 router.delete('/:userId', (req, res, next) => {
